b863223105ed01fe2d351612cd3e52a58af6ce19
smalldatascience <53816600+smalldatascience@users.noreply.github.com>
2026-03-30 14:46:29 +0200

    Add axis titles to the plot

--- analyse.R
+++ analyse.R
@@ -11,5 +11,7 @@ str(College)
 # Fit a linear model
 m1 <- lm(Outstate ~ Expend, data=College)
 
-plot(College$Expend, College$Outstate)
+plot(College$Expend, College$Outstate,
+     xlab = "Instructional expenditure per student",
+     ylab = "Out-of-state tuition")
 abline(m1, col = "red", lwd = 3)
