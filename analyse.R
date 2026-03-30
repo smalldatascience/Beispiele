742565108d6006b7b599e4e1338eeb317d83dab2
smalldatascience <53816600+smalldatascience@users.noreply.github.com>
2026-03-30 14:42:35 +0200

    First upload

---
+++ analyse.R
@@ -0,0 +1,15 @@
+# load package
+library(ISLR)
+data(College, package="ISLR")
+
+# check if any observation contains missing info
+identical(na.omit(College),College)
+
+# ?College
+str(College)
+
+# Fit a linear model
+m1 <- lm(Outstate ~ Expend, data=College)
+
+plot(College$Expend, College$Outstate)
+abline(m1, col = "red", lwd = 3)
